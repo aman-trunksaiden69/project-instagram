80f156f29bf8460f9bb7f5838795167626a8da06
aman qureshi <>
2025-04-07 02:59:13 -0700

    Initial-Stages

--- app.js
+++ app.js
@@ -44,6 +44,9 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+app.get('*', (req, res) => {
+  res.render("error"); // error.ejs bana le
+})
 
 // error handler
 app.use(function(err, req, res, next) {
