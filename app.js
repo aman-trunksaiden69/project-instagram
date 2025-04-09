a149b1c5533f194ef14603e4588813d2733a9a31
aman qureshi <>
2025-04-09 04:04:43 -0700

    Initial-Stages

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
+//error handler-
 app.use(function(err, req, res, next) {
   // Log error details safely in production
   if (process.env.NODE_ENV === 'production') {
@@ -54,8 +54,6 @@ app.use(function(err, req, res, next) {
     fs.appendFileSync('error.log', `${new Date()} - ${err.message}\n`); // Logs to file
   }
 
-// error handler
-app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
